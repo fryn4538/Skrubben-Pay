c6f64093c41035a5c84f6bcd40547749cc1a8029
fryn4538 <fredrik.yngve.4538@student.uu.se>
2020-09-28 16:46:55 +0200

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Item from './Item';
+import {listOfItems} from './items.js';
 
 function App() {
+  var cost = 0;
+  var msg = "";
+  
+  function onSwish(){
+    window.location = 'swish://payment?data=%7B%22version%22%3A1%2C%22payee%22%3A%7B%22value%22%3A%20%221236130983%22%7D%2C%22amount%22%3A%7B%22value%22%3A' + '10'+'%7D%2C%22message%22%3A%7B%22value%22%3A%22'+ 'Hello' +'%22%7D%7D'
+    return;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Skrubben-Pay</h1>
       </header>
+      {listOfItems.map((item,key) =>(
+        <Item 
+          name={item.name}
+          price={item.price}
+          img={item.img}
+          id={item.id}
+        />
+      ))}
+      <footer>
+        <button onClick={onSwish}>Swish</button>
+      </footer>
     </div>
+    
   );
 }
 
---
+++ src/items.js
@@ -0,0 +1,115 @@
+export const listOfItems = [
+  {
+    name: "Delicato Blå",
+    price: "8",
+    id: "db",
+    img: "img/",
+  },
+  {
+    name: "Delicato Blå",
+    price: "8",
+    id: "db",
+    img: "img/",
+  },
+  {
+    name: "Delicato Blå",
+    price: "8",
+    id: "db",
+    img: "img/",
+  },
+  {
+    name: "Delicato Blå",
+    price: "8",
+    id: "db",
+    img: "img/",
+  },
+  {
+    name: "Delicato Blå",
+    price: "8",
+    id: "db",
+    img: "img/",
+  },
+  {
+    name: "Delicato Blå",
+    price: "8",
+    id: "db",
+    img: "img/",
+  },
+  {
+    name: "Delicato Blå",
+    price: "8",
+    id: "db",
+    img: "img/",
+  },
+  {
+    name: "Delicato Blå",
+    price: "8",
+    id: "db",
+    img: "img/",
+  },
+  {
+    name: "Delicato Blå",
+    price: "8",
+    id: "db",
+    img: "img/",
+  },
+  {
+    name: "Delicato Blå",
+    price: "8",
+    id: "db",
+    img: "img/",
+  },
+  {
+    name: "Delicato Blå",
+    price: "8",
+    id: "db",
+    img: "img/",
+  },
+  {
+    name: "Delicato Blå",
+    price: "8",
+    id: "db",
+    img: "img/",
+  },
+  {
+    name: "Delicato Blå",
+    price: "8",
+    id: "db",
+    img: "img/",
+  },
+  {
+    name: "Delicato Blå",
+    price: "8",
+    id: "db",
+    img: "img/",
+  },
+  {
+    name: "Delicato Blå",
+    price: "8",
+    id: "db",
+    img: "img/",
+  },
+  {
+    name: "Delicato Blå",
+    price: "8",
+    id: "db",
+    img: "img/",
+  },
+  {
+    name: "Delicato Blå",
+    price: "8",
+    id: "db",
+    img: "img/",
+  },
+  {
+    name: "Delicato Blå",
+    price: "8",
+    id: "db",
+    img: "img/",
+  },
+
+
+
+
+
+];
