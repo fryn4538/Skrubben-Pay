a5673b22095c0af495f08f6a029c3a751c79982c
fryn4538 <fredrik.yngve.4538@student.uu.se>
2020-10-06 11:03:33 +0200

    Updates

---
+++ precache-manifest.70c8a5c9ded75b8445f22aec148bb766.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f05e208e56ed73bd09ac7fac928a6cf3",
+    "url": "/Skrubben-Pay/index.html"
+  },
+  {
+    "revision": "1f8b90e8860240c0be74",
+    "url": "/Skrubben-Pay/static/css/main.b9be1e63.chunk.css"
+  },
+  {
+    "revision": "f4d57554ac186a92215f",
+    "url": "/Skrubben-Pay/static/js/2.7417b907.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Skrubben-Pay/static/js/2.7417b907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f8b90e8860240c0be74",
+    "url": "/Skrubben-Pay/static/js/main.a9ff751a.chunk.js"
+  },
+  {
+    "revision": "e86c5bb3a39b1ba8060f",
+    "url": "/Skrubben-Pay/static/js/runtime-main.3329310a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Skrubben-Pay/precache-manifest.5238656a809b838f8418d8b55c1bd6d5.js"
+  "/Skrubben-Pay/precache-manifest.70c8a5c9ded75b8445f22aec148bb766.js"
 );
 
 self.addEventListener('message', (event) => {
