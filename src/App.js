c7af31fe664e1b3c138bb2732ff1162b4e7e585e
fryn4538 <fryn4538@student.uu.se>
2021-01-25 22:08:49 +0100

    fixed security

--- src/App.js
+++ src/App.js
@@ -10,13 +10,11 @@ import { faUndoAlt } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 import {listOfItems} from './items.js';
 
-
-
 function App() {
 
   const [toSwish, setToSwish] = useState(false);
   const [cart, setCart] = useState([]);
-  const [cost, setCost] = useState(0);  
+  const [cost, setCost] = useState(0);
   const [items, setItems] = useState(listOfItems);
 
 
@@ -35,9 +33,9 @@ function App() {
          }
       }
       setItems(dispCart);
-      setToSwish(true);   
+      setToSwish(true);
     } else {
-      if(isMobile){ 
+      if(isMobile){
         console.log(isMobile);
         var price = 0;
         var msg = "";
@@ -58,15 +56,15 @@ function App() {
           alert("Too many items added!");
         } else {
           var tStart = new Date().getTime();
-          window.location = 'swish://payment?data=%7B%22version%22%3A1%2C%22payee%22%3A%7B%22value%22%3A%20%221236130983%22%7D%2C%22amount%22%3A%7B%22value%22%3A' + price.toString() + '%7D%2C%22message%22%3A%7B%22value%22%3A%22'+ msg +'%22%7D%7D'
+          window.location = process.env.REACT_APP_FST + price.toString() + process.env.REACT_APP_SND + msg + process.env.REACT_APP_THR;
           var tEnd = new Date().getTime();
         }
       } else {
         alert("You have to be on a mobile device");
-      }     
+      }
       onBack();
     }
-    
+
     return;
   }
 
@@ -75,7 +73,7 @@ function App() {
     setCost(0);
     setItems(listOfItems);
     document.getElementById("searchInput").value = "";
-   
+
     return;
   }
 
@@ -84,7 +82,7 @@ function App() {
     setItems(listOfItems);
     setToSwish(false);
     document.getElementById("searchInput").value = "";
-    
+
     return;
   }
 
@@ -108,20 +106,20 @@ function App() {
       setCart([...cart,product]);
       setCost(cost + Number(product.price));
   }
-  
+
   const numOfItems = (product) => {
       var count = 0;
       for(var i = 0; i < cart.length; ++i){
         if(cart[i] === product){
-          count++; 
+          count++;
         }
-      } 
+      }
       return count;
   }
 
   const seachFilter = event => {
     const tmp = [];
-    const str = event.target.value.toUpperCase(); 
+    const str = event.target.value.toUpperCase();
     for(var i = 0; i<listOfItems.length; ++i){
         if(listOfItems[i].name.toUpperCase().includes(str)){
             tmp.push(listOfItems[i]);
@@ -137,7 +135,7 @@ function App() {
         <h1>Skrubben-Pay</h1>
         <h2 className={toSwish === false ? 'hidden' : ''}>In cart</h2>
       </header>
-      
+
       <div >
         <input type="text" id="searchInput" className={toSwish ? "hidden" : "search inline"} onChange={seachFilter} name="fname" placeholder="Search for items"/>
       </div>
@@ -147,7 +145,7 @@ function App() {
           <div className='card'>
             <img className='size' src={item.img} alt=""></img>
           </div>
-          
+
           <p className='card'>{item.price} kr</p>
 
           <div className='card'>
@@ -172,7 +170,7 @@ function App() {
         <a className={toSwish ? "" : "hidden"} onClick={onSwish}><FontAwesomeIcon icon={faCoins} href="#" />  Swish</a>
       </footer>
     </div>
-    
+
   );
 }
 
